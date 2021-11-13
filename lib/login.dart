9218ddcdc10d352ef7126d0fff5756b0c03fbee7
ihebessaied <i.essaied@yahoo.fr>
2021-11-13 11:27:02 +0100

    add text field widgets

--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,17 @@
+// Copyright 2018-present the Flutter authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -24,8 +38,13 @@ class _LoginPageState extends State<LoginPage> {
                 const Text('SHRINE'),
               ],
             ),
-            const SizedBox(height: 120.0),const TextField(
-              decoration: InputDecoration(
+            const SizedBox(height: 120.0),
+            // TODO: Wrap Username with AccentColorOverride (103)
+            // TODO: Remove filled: true values (103)
+            // TODO: Wrap Password with AccentColorOverride (103)
+            // TODO: Add TextField widgets (101)
+            TextField(
+              decoration: const InputDecoration(
                 filled: true,
                 labelText: 'Username',
               ),
@@ -33,18 +52,13 @@ class _LoginPageState extends State<LoginPage> {
 // spacer
             const SizedBox(height: 12.0),
 // [Password]
-            const TextField(
-              decoration: InputDecoration(
+            TextField(
+              decoration: const InputDecoration(
                 filled: true,
                 labelText: 'Password',
               ),
               obscureText: true,
             ),
-
-            // TODO: Wrap Username with AccentColorOverride (103)
-            // TODO: Remove filled: true values (103)
-            // TODO: Wrap Password with AccentColorOverride (103)
-            // TODO: Add TextField widgets (101)
             // TODO: Add button bar (101)
           ],
         ),
