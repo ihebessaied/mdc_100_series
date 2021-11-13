5acc5dbe4449cd0965c38ece13ed9238bf12a13f
ihebessaied <i.essaied@yahoo.fr>
2021-11-13 16:22:47 +0100

    add a menu on the bacl layer

--- lib/home.dart
+++ lib/home.dart
@@ -7,11 +7,16 @@ import 'supplemental/asymmetric_view.dart';
 
 class HomePage extends StatelessWidget {
   // TODO: Add a variable for Category (104)
+  final Category category;
+
+  const HomePage({this.category = Category.all, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
+    return AsymmetricView(products: ProductsRepository.loadProducts(category));
+
     return Scaffold(
       appBar: AppBar(
         systemOverlayStyle: SystemUiOverlayStyle.dark,
