b5c177902ab46fbaa267ef612f35fd6c2df33aaf
ihebessaied <i.essaied@yahoo.fr>
2021-11-13 13:56:23 +0100

    modify typography and label styles

--- lib/login.dart
+++ lib/login.dart
@@ -25,6 +25,23 @@ class _LoginPageState extends State<LoginPage> {
   // TODO: Add text editing controllers (101)
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _unfocusedColor = Colors.grey[600];
+  final _usernameFocusNode = FocusNode();
+  final _passwordFocusNode = FocusNode();
+  @override
+  void initState() {
+    super.initState();
+    _usernameFocusNode.addListener(() {
+      setState(() {
+        //Redraw so that the username label reflects the focus state
+      });
+    });
+    _passwordFocusNode.addListener(() {
+      setState(() {
+        //Redraw so that the password label reflects the focus state
+      });
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,32 +54,39 @@ class _LoginPageState extends State<LoginPage> {
               children: <Widget>[
                 Image.asset('assets/diamond.png'),
                 const SizedBox(height: 16.0),
-                const Text('SHRINE'),
+                Text(
+                  'SHRINE',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
               ],
             ),
             const SizedBox(height: 120.0),
             // TODO: Wrap Username with AccentColorOverride (103)
             // TODO: Remove filled: true values (103)
-            // TODO: Wrap Password with AccentColorOverride (103)
-            // TODO: Add TextField widgets (101)
-            TextField(
-              decoration: const InputDecoration(
-                filled: true,
-                labelText: 'Username',
-              ),
-              controller: _usernameController,
-            ),
-// spacer
-            const SizedBox(height: 12.0),
-// [Password]
-            TextField(
-              decoration: const InputDecoration(
-                filled: true,
-                labelText: 'Password',
-              ),
-              controller: _passwordController,
-              obscureText: true,
-            ),
+           
+        TextField(
+          controller: _usernameController,
+          decoration: InputDecoration(
+            labelText: 'Username',
+            labelStyle: TextStyle(
+                color: _usernameFocusNode.hasFocus
+                    ? Theme.of(context).colorScheme.secondary
+                    : _unfocusedColor),
+          ),
+          focusNode: _usernameFocusNode,
+        ),
+        const SizedBox(height: 12.0),
+        TextField(
+          controller: _passwordController,
+          decoration: InputDecoration(
+            labelText: 'Password',
+            labelStyle: TextStyle(
+                color: _passwordFocusNode.hasFocus
+                    ? Theme.of(context).colorScheme.secondary
+                    : _unfocusedColor),
+          ),
+          focusNode: _passwordFocusNode,
+        ),
             // TODO: Add button bar (101)
             ButtonBar(
               // TODO: Add a beveled rectangular border to CANCEL (103)
