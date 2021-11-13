eb23d3bde377ee41cb6048125c6f7d55fca2ae55
ihebessaied <i.essaied@yahoo.fr>
2021-11-13 12:18:57 +0100

    add gridview

--- lib/home.dart
+++ lib/home.dart
@@ -62,8 +62,12 @@ class HomePage extends StatelessWidget {
       ),
 
       // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        // TODO: Build a grid of cards (102)
+        children: <Widget>[Card()],
       ),
       // TODO: Set resizeToAvoidBottomInset (101)
       resizeToAvoidBottomInset: false,
